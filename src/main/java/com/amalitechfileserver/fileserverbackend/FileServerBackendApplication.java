8f43edd7f1fb85b3853f1f88a3b6d991d4766bd2
GideonAg <gideonagbosu72@gmail.com>
2024-05-04 00:34:41 +0000

    Added project starter

---
+++ src/main/java/com/amalitechfileserver/fileserverbackend/FileServerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.amalitechfileserver.fileserverbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileServerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileServerBackendApplication.class, args);
+	}
+
+}
